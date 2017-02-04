00ae819581fc0ddb4dda01ded8986b0997349056
Nethermaker <tjsenne@yahoo.com>
2017-02-04 12:56:34 -0600

    Finish codebreaker.py

--- intro/codebreaker.py
+++ intro/codebreaker.py
@@ -1,5 +1,3 @@
-
-
 def rotate_letter(letter, number):
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     if letter.isalpha():
@@ -28,7 +26,78 @@ def multiple_caesar(string):
         print caesar_cipher(string, count, True)
         count += 1
 
-#Code 1: Great job at figuring out how to crack the first code. To solve the other one, the
+#CODE 1: Great job at figuring out how to crack the first code. To solve the other one, the
 # keyword is the last name of the author of the novels The Shining, Misery, and Carrie.
 
 #Keyword = King
+
+def letter_value(letter):
+    if letter.isupper():
+        return ord(letter) - 64
+    else:
+        return ord(letter) - 96
+
+def fill_out(string, length):
+    new_string = ''
+    count = 0
+    while len(new_string) < length:
+        new_string = new_string + string[count % len(string)]
+        count += 1
+    return new_string
+
+def remove_extras(string):
+    for letter in string:
+        if not letter.isalpha():
+            string = string.replace(letter, '')
+    return string.lower()
+
+#Commented-out lines were used for debugging
+def vigenere(string, keyword, encode):
+    string = remove_extras(string)
+    #print string
+    
+    key = fill_out(keyword, len(string)).lower()
+    #print key
+    
+    key_num = []
+    for letter in key:
+        if not encode:
+            key_num.append(-(letter_value(letter)))
+        else:
+            key_num.append(letter_value(letter))
+    #print key_num
+
+    final_string =  ''
+    #print zip(string, key_num)
+    for letter, num in zip(string, key_num):
+        final_string += rotate_letter(letter, num)
+    return final_string
+
+#CODE 2: congratulationsyouhavemanagedtocrackthecodeyouareclearlytoosmartforthisclass
+#
+#or...: Congratulations you have managed to crack the code you are clearly
+#       too smart for this class
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
