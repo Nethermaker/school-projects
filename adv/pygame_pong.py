858b1fd2743b28d9993d9950469d487a57becb4c
Nethermaker <tjsenne@yahoo.com>
2017-04-13 09:33:21 -0500

    finish pong and some other stuff (?) 4/13/17

--- adv/pygame_pong.py
+++ adv/pygame_pong.py
@@ -3,6 +3,34 @@ import pygame, random, math
 from pygame.locals import *
 
 
+class Particle(pygame.sprite.Sprite):
+
+    def __init__(self, pos):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.rect = Rect(0,0,5,5)
+        self.rect.center = pos
+
+        self.image = pygame.Surface(self.rect.size)
+        self.image.fill(Color('yellow'))
+
+        self.velx = random.randint(-5, 5)
+        self.vely = random.randint(-5, 5)
+
+
+    def update(self, screen):
+
+        self.vely += 1
+
+        self.rect.x += self.velx
+        self.rect.y += self.vely
+
+        if self.rect.y > screen.get_height():
+            self.kill()
+        
+        
+        
+
 class Paddle(pygame.sprite.Sprite):
     WIDTH, HEIGHT = 15, 150
     SPEED = 20
@@ -29,16 +57,56 @@ class Paddle(pygame.sprite.Sprite):
 
 
 class Ball(pygame.sprite.Sprite):
-    SPEED = 5
+    SPEED = 10
 
-    def __init__(self):
+    def __init__(self, screen):
         pygame.sprite.Sprite.__init__(self)
 
-        self. rect = Rect(400,300,20,20)
+        self.screen = screen
 
+        self.rect = Rect(400,300,20,20)
         self.image = pygame.Surface(self.rect.size)
         self.image.fill(Color('orange'))
         
+        self.velx = self.SPEED
+        self.vely = self.SPEED
+    
+    def update(self, game):
+        self.rect.x += self.velx
+        self.rect.y += self.vely
+
+        if self.rect.top <= 0:
+            self.vely *= -1
+        if self.rect.bottom >= self.screen.get_height():
+            self.vely *= -1
+
+        if not self.rect.colliderect(self.screen.get_rect()):
+            if self.rect.x >= self.screen.get_width()/2:
+                game.left_score += 1
+            elif self.rect.x <= self.screen.get_width()/2:
+                game.right_score += 1
+            self.reset()
+
+    def handle_collision(self, paddle, game):
+        if self.velx < 0:   #If the ball is going to the left
+            self.rect.left = paddle.rect.right
+            self.velx *= -1
+            self.vely += random.randint(-3,3)
+        elif self.velx > 0: #If the ball is going to the right
+            self.rect.right = paddle.rect.left
+            self.velx *= -1
+            self.vely += random.randint(-3,3)
+        for x in range(0,random.randint(5,10)):
+            game.particles.add(Particle(self.rect.center))
+        
+
+    def reset(self):
+        self.rect.center = (self.screen.get_width()/2, self.screen.get_height()/2)
+        self.velx = 0
+        self.vely = 0
+
+
+    
 
 
 
@@ -60,13 +128,37 @@ class GameMain():
         self.paddles = pygame.sprite.Group()
         self.paddles.add(self.player1, self.player2) #put the paddles in a group
 
-        self.ball = Ball()
+        self.ball = Ball(self.screen)
         self.balls = pygame.sprite.Group()
         self.balls.add(self.ball)
+
+        self.left_score = 0
+        self.right_score = 0
+
+        self.particles = pygame.sprite.Group()
+
+        self.font = pygame.font.Font('freesansbold.ttf', 32)
+        
+        self.left_score_text = self.font.render('{}'.format(self.left_score), True, Color('red'))
+        self.left_score_rect = self.left_score_text.get_rect()
+        self.left_score_rect.left = 10
+        self.left_score_rect.top = 10
+
+        self.right_score_text = self.font.render('{}'.format(self.right_score), True, Color('red'))
+        self.right_score_rect = self.right_score_text.get_rect()
+        self.right_score_rect.right = self.width - 10
+        self.right_score_rect.top = 10
 
     def main_loop(self):
         while not self.done:
             self.handle_events()
+            self.balls.update(game)
+            self.particles.update(self.screen)
+
+            for p in self.paddles:
+                if self.ball.rect.colliderect(p.rect):
+                    self.ball.handle_collision(p, game)
+                    
             self.draw()
             self.clock.tick(60)
         pygame.quit()
@@ -77,6 +169,13 @@ class GameMain():
         self.paddles.draw(self.screen)
 
         self.balls.draw(self.screen)
+
+        self.particles.draw(self.screen)
+
+        self.left_score_text = self.font.render('{}'.format(self.left_score), True, Color('red'))
+        self.screen.blit(self.left_score_text, self.left_score_rect)
+        self.right_score_text = self.font.render('{}'.format(self.right_score), True, Color('red'))
+        self.screen.blit(self.right_score_text, self.right_score_rect)
 
         pygame.display.flip() #put all the work on the screen
 
@@ -95,6 +194,14 @@ class GameMain():
             elif event.type == KEYDOWN:
                 if event.key == K_ESCAPE:
                     self.done = True
+                elif event.key == K_SPACE and self.ball.velx == 0 and self.ball.vely == 0:
+                    ranges = range(0,61) + range(120,241) + range(300, 360)
+                    angle = math.radians(random.choice(ranges))
+                    self.ball.velx = math.cos(angle) * self.ball.SPEED
+                    self.ball.vely = math.sin(angle) * self.ball.SPEED
+                elif event.key == K_SPACE:
+                    self.ball.reset()
+                
                 
 
 if __name__ == '__main__':
--- adv/vending_machine.py
+++ adv/vending_machine.py
@@ -1,3 +1,5 @@
+import random
+
 class Vending:
 
     def __init__(self, cookies_count, water_count, chips_count):
@@ -8,7 +10,7 @@ class Vending:
         self.water_c = 0.99
         self.chips = chips_count
         self.chips_c = 1.50
-        self.pennies = 100
+        self.pennies = 50
         self.nickels = 20
         self.dimes = 20
         self.quarters = 30
@@ -30,6 +32,11 @@ class Vending:
                      (ones*1.0) + (fives*5.0) + (tens*10.0)
         print 'You have entered {} so far.'.format(self.user)
 
+    def machine_total(self):
+        print 'The machine contains ${}'.format(self.tens*10.0 + self.fives*5.0 + self.ones*1.0 \
+                                                + self.quarters*0.25 + self.dimes*0.1 + \
+                                                self.nickels*0.05 + self.pennies*0.01)
+
     def get_cookies(self):
         '''Purchase some cookies'''
         if self.cookies == 0:
@@ -47,7 +54,7 @@ class Vending:
             nickels = 0
             pennies = 0
             while self.user > 0.0:
-                print self.user
+                self.user = round(self.user, 2)
                 if self.user >= 10.0 and self.tens > 0:
                     tens += 1
                     self.tens -= 1
@@ -76,56 +83,286 @@ class Vending:
                     pennies += 1
                     self.pennies -= 1
                     self.user -= 0.01
+                else:
+                    break
+            self.cookies -= 1
             if self.user > 0:
+                self.cookies += 1
                 print 'Not enough money in machine for change. Please enter exact change.'
                 self.user = temp_user
-                tens = 0
-                fives = 0
-                ones = 0
-                quarters = 0
-                dimes = 0
-                nickels = 0
-                pennies = 0
+                self.tens += tens
+                self.fives += fives
+                self.ones += ones
+                self.quarters += quarters
+                self.dimes += dimes
+                self.nickels += nickels
+                self.pennies += pennies
                 while self.user > 0:
+                    self.user = round(self.user, 2)
                     if self.user >= 10.0:
-                        tens += 1
                         self.tens -= 1
                         self.user -= 10.0
                     elif self.user >= 5.0:
-                        fives += 1
                         self.fives -= 1
                         self.user -= 5.0
                     elif self.user >= 1.0:
-                        ones += 1
                         self.ones -= 1
                         self.user -= 1.0
                     elif self.user >= 0.25:
-                        quarters += 1
                         self.quarters -= 1
                         self.user -= 0.25
                     elif self.user >= 0.1:
-                        dimes += 1
                         self.dimes -= 1
                         self.user -= 0.1
                     elif self.user >= 0.05:
-                        nickels += 1
                         self.nickels -= 1
                         self.user -= 0.05
                     elif self.user >= 0.01:
-                        pennies += 1
                         self.pennies -= 1
                         self.user -= 0.01
-                return 'You have gotten your money back.'
+                print 'You have gotten your money back.'
             else:
                 print 'Enjoy your snack!'
-                return '''Your change is:
+                print '''Your change is:
 Tens: {}
 Fives: {}
 Ones: {}
 Quarters: {}
 Dimes: {}
 Nickels: {}
-Pennies: {}.'''.format(tens, fives, ones, quarters, dimes, nickels, pennies)
+Pennies: {}'''.format(tens, fives, ones, quarters, dimes, nickels, pennies)
+
+    def get_chips(self):
+        '''Purchase some cookies'''
+        if self.chips == 0:
+            return 'Sorry, we are sold out of that item.'
+        elif self.user < self.chips_c:
+            return 'Those cost $1.95. Please enter more money.'
+        elif self.user >= self.chips_c:
+            temp_user = self.user
+            self.user -= self.chips_c
+            tens = 0
+            fives = 0
+            ones = 0
+            quarters = 0
+            dimes = 0
+            nickels = 0
+            pennies = 0
+            while self.user > 0.0:
+                self.user = round(self.user, 2)
+                if self.user >= 10.0 and self.tens > 0:
+                    tens += 1
+                    self.tens -= 1
+                    self.user -= 10.0
+                elif self.user >= 5.0 and self.fives > 0:
+                    fives += 1
+                    self.fives -= 1
+                    self.user -= 5.0
+                elif self.user >= 1.0 and self.ones > 0:
+                    ones += 1
+                    self.ones -= 1
+                    self.user -= 1.0
+                elif self.user >= 0.25 and self.quarters > 0:
+                    quarters += 1
+                    self.quarters -= 1
+                    self.user -= 0.25
+                elif self.user >= 0.1 and self.dimes > 0:
+                    dimes += 1
+                    self.dimes -= 1
+                    self.user -= 0.1
+                elif self.user >= 0.05 and self.nickels > 0:
+                    nickels += 1
+                    self.nickels -= 1
+                    self.user -= 0.05
+                elif self.user >= 0.01 and self.pennies > 0:
+                    pennies += 1
+                    self.pennies -= 1
+                    self.user -= 0.01
+                else:
+                    break
+            self.chips -= 1
+            if self.user > 0:
+                self.chips += 1
+                print 'Not enough money in machine for change. Please enter exact change.'
+                self.user = temp_user
+                self.tens += tens
+                self.fives += fives
+                self.ones += ones
+                self.quarters += quarters
+                self.dimes += dimes
+                self.nickels += nickels
+                self.pennies += pennies
+                while self.user > 0:
+                    self.user = round(self.user, 2)
+                    if self.user >= 10.0:
+                        self.tens -= 1
+                        self.user -= 10.0
+                    elif self.user >= 5.0:
+                        self.fives -= 1
+                        self.user -= 5.0
+                    elif self.user >= 1.0:
+                        self.ones -= 1
+                        self.user -= 1.0
+                    elif self.user >= 0.25:
+                        self.quarters -= 1
+                        self.user -= 0.25
+                    elif self.user >= 0.1:
+                        self.dimes -= 1
+                        self.user -= 0.1
+                    elif self.user >= 0.05:
+                        self.nickels -= 1
+                        self.user -= 0.05
+                    elif self.user >= 0.01:
+                        self.pennies -= 1
+                        self.user -= 0.01
+                print 'You have gotten your money back.'
+            else:
+                print 'Enjoy your snack!'
+                print '''Your change is:
+Tens: {}
+Fives: {}
+Ones: {}
+Quarters: {}
+Dimes: {}
+Nickels: {}
+Pennies: {}'''.format(tens, fives, ones, quarters, dimes, nickels, pennies)
+
+    def get_water(self):
+        '''Purchase some cookies'''
+        if self.water == 0:
+            return 'Sorry, we are sold out of that item.'
+        elif self.user < self.water_c:
+            return 'Those cost $1.95. Please enter more money.'
+        elif self.user >= self.water_c:
+            temp_user = self.user
+            self.user -= self.water_c
+            tens = 0
+            fives = 0
+            ones = 0
+            quarters = 0
+            dimes = 0
+            nickels = 0
+            pennies = 0
+            while self.user > 0.0:
+                self.user = round(self.user, 2)
+                if self.user >= 10.0 and self.tens > 0:
+                    tens += 1
+                    self.tens -= 1
+                    self.user -= 10.0
+                elif self.user >= 5.0 and self.fives > 0:
+                    fives += 1
+                    self.fives -= 1
+                    self.user -= 5.0
+                elif self.user >= 1.0 and self.ones > 0:
+                    ones += 1
+                    self.ones -= 1
+                    self.user -= 1.0
+                elif self.user >= 0.25 and self.quarters > 0:
+                    quarters += 1
+                    self.quarters -= 1
+                    self.user -= 0.25
+                elif self.user >= 0.1 and self.dimes > 0:
+                    dimes += 1
+                    self.dimes -= 1
+                    self.user -= 0.1
+                elif self.user >= 0.05 and self.nickels > 0:
+                    nickels += 1
+                    self.nickels -= 1
+                    self.user -= 0.05
+                elif self.user >= 0.01 and self.pennies > 0:
+                    pennies += 1
+                    self.pennies -= 1
+                    self.user -= 0.01
+                else:
+                    break
+            self.water -= 1
+            if self.user > 0:
+                self.water += 1
+                print 'Not enough money in machine for change. Please enter exact change.'
+                self.user = temp_user
+                self.tens += tens
+                self.fives += fives
+                self.ones += ones
+                self.quarters += quarters
+                self.dimes += dimes
+                self.nickels += nickels
+                self.pennies += pennies
+                while self.user > 0:
+                    self.user = round(self.user, 2)
+                    if self.user >= 10.0:
+                        self.tens -= 1
+                        self.user -= 10.0
+                    elif self.user >= 5.0:
+                        self.fives -= 1
+                        self.user -= 5.0
+                    elif self.user >= 1.0:
+                        self.ones -= 1
+                        self.user -= 1.0
+                    elif self.user >= 0.25:
+                        self.quarters -= 1
+                        self.user -= 0.25
+                    elif self.user >= 0.1:
+                        self.dimes -= 1
+                        self.user -= 0.1
+                    elif self.user >= 0.05:
+                        self.nickels -= 1
+                        self.user -= 0.05
+                    elif self.user >= 0.01:
+                        self.pennies -= 1
+                        self.user -= 0.01
+                print 'You have gotten your money back.'
+            else:
+                print 'Enjoy your snack!'
+                print '''Your change is:
+Tens: {}
+Fives: {}
+Ones: {}
+Quarters: {}
+Dimes: {}
+Nickels: {}
+Pennies: {}'''.format(tens, fives, ones, quarters, dimes, nickels, pennies)
+
+
+    def shake(self):
+        print 'You shake the vending machine! Cheater...'
+        chance = random.randint(0,3)
+        if chance == 0:
+            print 'A bag of chips falls to the bottom.'
+            self.chips -= 1
+        elif chance == 1:
+            print 'A package of cookies falls to the bottom.'
+            self.cookies -= 1
+        elif chance == 2:
+            print 'A bottle of water falls to the bottom.'
+            self.water -= 1
+        elif chance == 3:
+            print 'You hear someone come up behind you.'
+            print '"Hey! That\'s called stealing, you know!"'
+            print 'The police have been called to your location.'
+            print 'You will rot in prison for the rest of your life.'
+            while True:
+                pass
+            
+        
+
+    def __str__(self):
+        print '''This vending machine has:
+Cookies: {}
+Chips: {}
+Waters: {}
+
+CHANGE:
+Tens: {}
+Fives: {}
+Ones: {}
+Quarters: {}
+Dimes: {}
+Nickels: {}
+Pennies: {}'''.format(self.cookies, self.chips, self.water, self.tens, self.fives,
+                      self.ones, self.quarters, self.dimes, self.nickels, self.pennies)
+        return ''
+
+
                 
     
         
@@ -146,3 +383,4 @@ Pennies: {}.'''.format(tens, fives, ones, quarters, dimes, nickels, pennies)
 
 
 vending = Vending(15, 10, 20)
+print 'Welcome to the vending machine!'
