baf2d8706c668662a25ee1995d49c69348b4ac4c
Nethermaker <tjsenne@yahoo.com>
2017-04-26 09:45:49 -0500

    Add files via upload

--- adv/gh3/gh3.py
+++ adv/gh3/gh3.py
@@ -2,27 +2,139 @@ import pygame, random, math
 
 from pygame.locals import *
 
+
+class Note(pygame.sprite.Sprite):
+
+    def __init__(self, tick, y, color, hopo=False, hold=0):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.tick = tick
+        self.color = color
+        self.hopo = hopo
+        self.hold = hold
+
+        self.y = y
+        self.x = 0
+
+        if self.color == 'green':
+            self.x = 540
+        elif self.color == 'red':
+            self.x = 590
+        elif self.color == 'yellow':
+            self.x = 640
+        elif self.color == 'blue':
+            self.x = 690
+        elif self.color == 'orange':
+            self.x = 740
+
+        self.image = pygame.Surface([40,40])
+
+        self.rect = self.image.get_rect()
+        self.rect.center = (self.x, self.y)
+
+    def update(self):
+
+        self.rect.y += 4
+
+
+class Song:
+
+    def __init__(self, filename):
+
+        self.chart = filename
+        self.song = ''
+
+        self.resolution = 0
+        self.offset = 0
+        self.current_tick = 0
+        
+        self.current_bpm = 0
+        self.bpm_dict = {}
+
+        self.last_note_hit = False
+
+        #self.note_list = [Note(192, 400, 'green', hopo=True),
+                          #Note(284, 300, 'blue')]
+        self.note_list = []
+
+    def load_chart(self):
+        with open(self.chart, 'rb') as infile:
+            for line in infile:
+                line = line.strip()
+                if 'Resolution' in line:
+                    self.resolution = int(line[13:])
+                elif 'Offset' in line:
+                    self.offset = int(line[9:])
+                elif ' N ' in line:
+                    line = line.split()
+                    
+
+        
+
+class Fret(pygame.sprite.Sprite):
+
+    def __init__(self, x, y, color):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.x = x
+        self.y = y
+        self.color = color
+
+        self.image = pygame.Surface([50,50])
+
+        self.rect = self.image.get_rect()
+        self.rect.center = (self.x, self.y)
+
+        self.pressed = False   
+
 class GameMain():
 
     done = False
-    color_bg = Color('gray30')
+    color_bg = Color('black')
 
     def __init__(self, width=1280, height=800):
-
+        
+        pygame.mixer.pre_init(44100,-16,2,2048)
         pygame.init()
         self.width, self.height = width, height
         self.screen = pygame.display.set_mode((self.width, self.height))
         self.clock = pygame.time.Clock()
 
+        self.frets = pygame.sprite.Group()
+        self.fret0 = Fret(540, 720, Color('green'))
+        self.fret1 = Fret(590, 720, Color('red'))
+        self.fret2 = Fret(640, 720, Color('yellow'))
+        self.fret3 = Fret(690, 720, Color('blue'))
+        self.fret4 = Fret(740, 720, Color('orange'))
+
+        self.frets.add(self.fret0, self.fret1, self.fret2, self.fret3, self.fret4)
+
+        self.song = Song('test.chart')
+        self.song.load_chart()
+
     def main_loop(self):
         while not self.done:
             self.handle_events()
+            for note in self.song.note_list:
+                note.update()
             self.draw()
             self.clock.tick(60)
         pygame.quit()
 
     def draw(self):
         self.screen.fill(self.color_bg)
+
+        for fret in self.frets:
+            pygame.draw.circle(self.screen, fret.color, fret.rect.center, 25)
+            if fret.pressed:
+                pygame.draw.circle(self.screen, Color('black'), fret.rect.center, 15)
+
+        for note in self.song.note_list:
+            pygame.draw.circle(self.screen, Color(note.color), note.rect.center, 20)
+            if note.hopo == True:
+                pygame.draw.circle(self.screen, Color('white'), note.rect.center, 10)
+                
+        
 
         pygame.display.flip() #put all the work on the screen
 
@@ -37,7 +149,48 @@ class GameMain():
             elif event.type == KEYDOWN:
                 if event.key == K_ESCAPE:
                     self.done = True
+                elif event.key == K_a:
+                    self.fret0.pressed = True
+                elif event.key == K_s:
+                    self.fret1.pressed = True
+                elif event.key == K_d:
+                    self.fret2.pressed = True
+                elif event.key == K_f:
+                    self.fret3.pressed = True
+                elif event.key == K_g:
+                    self.fret4.pressed = True
+                elif event.key == K_LEFT or event.key == K_RIGHT:
+                    print 'strum'
+            elif event.type == KEYUP:
+                if event.key == K_a:
+                    self.fret0.pressed = False
+                elif event.key == K_s:
+                    self.fret1.pressed = False
+                elif event.key == K_d:
+                    self.fret2.pressed = False
+                elif event.key == K_f:
+                    self.fret3.pressed = False
+                elif event.key == K_g:
+                    self.fret4.pressed = False
 
 if __name__ == '__main__':
     game = GameMain()
     game.main_loop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
